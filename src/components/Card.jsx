d104c5ed565ff6045030b0cad160a4a6050ffc0b
ramil_novruzov <ramil.novruzov@gmx.de>
2025-02-26 16:53:03 +0100

    added card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,15 @@
 const Card = (props) => {
   return (
-    <div className="flex flex-col items-start p-4 border rounded-lg shadow-lg">
-      <span>Datum</span>
-      <h2>Post Title</h2>
+    <div className="flex flex-col items-start  border rounded-lg shadow-lg bg-amber-100 w-[300px] md:w-[400px] lg:w-[500px]">
+      <div className="card-title p-4">
+        <span>{props.datum}</span>
+        <h2 className="text-lg md:text-xl font-bold">A hike on the mountain</h2>
+      </div>
+
       <img
         src="https://picsum.photos/id/237/200/300"
         alt=""
-        className="h-[200px] w-full object-cover"
+        className="h-[200px] w-full object-cover rounded-b-lg"
       />
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,9 @@ import Card from "../components/Card";
 
 const HomePage = (props) => {
   return (
-    <div>
+    <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Card></Card>
+      <Card></Card>
       <Card></Card>
     </div>
   );
