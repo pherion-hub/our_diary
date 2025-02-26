82c6ebfbcab64366b3d58e909073b6f99c014b29
ramil_novruzov <ramil.novruzov@gmx.de>
2025-02-26 16:16:47 +0100

    wip

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,11 @@ const Card = (props) => {
     <div className="flex flex-col items-start p-4 border rounded-lg shadow-lg">
       <span>Datum</span>
       <h2>Post Title</h2>
-      <img src="https://picsum.photos/id/237/200/300" alt="" />
+      <img
+        src="https://picsum.photos/id/237/200/300"
+        alt=""
+        className="h-[200px] w-full object-cover"
+      />
     </div>
   );
 };
